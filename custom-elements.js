4fd1eb56f499a5dfcca6d1a77e062f87edfa5bfb
findus11 <findusxi@protonmail.com>
2022-06-07 20:39:34 +0200

    cleanup, remove custom-elements.js

--- custom-elements.js
+++
@@ -1,8 +0,0 @@
-document.onload = () => {
-  const elems = ['hi-co', 'hi-kw', 'hi-li', 'hi-ty'];
-  for (const elem of elems) {
-    document.registerElement(elem, {
-      prototype: Object.create(HTMLElement.prototype),
-    });
-  }
-};
